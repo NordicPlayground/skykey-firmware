8b5b7b6f536b3544a7ee8f9fad25f9dab57e41b9
Embla-Flatlandsmo <embla.flatlandsmo@nordicsemi.no>
2021-06-17 08:53:44 +0200

    use click events in stead of button events

--- configuration/nrf52840dk_nrf52840/click_detector_def.h
+++ configuration/nrf52840dk_nrf52840/click_detector_def.h
@@ -5,7 +5,7 @@
  */
 
 #include <caf/click_detector.h>
-
+#include <caf/key_id.h>
 /* This configuration file is included only once from click_detector module
  * and holds information about click detector configuration.
  */
@@ -17,7 +17,11 @@ const struct {} click_detector_def_include_once;
 
 static const struct click_detector_config click_detector_config[] = {
 	{
-		.key_id = KEY_ID(0,1),
+		.key_id = KEY_ID(0x00,0x01),
 		.consume_button_event = true,
 	},
+    {
+        .key_id = KEY_ID(0x00, 0x03),
+        .consume_button_event = true,
+    }
 };
--- src/modules/test.c
+++ src/modules/test.c
@@ -6,7 +6,7 @@
 
 #include <zephyr.h>
 #include "event_manager.h"
-#include <caf/events/button_event.h>
+#include <caf/events/click_event.h>
 
 
 #define MODULE test
@@ -16,13 +16,12 @@ LOG_MODULE_REGISTER(MODULE);
 
 static bool event_handler(const struct event_header *eh)
 {
-	if (is_button_event(eh)) {
-		const struct button_event *event = cast_button_event(eh);
-        LOG_INF("Button event from key_id: %d", event->key_id);
+	if (is_click_event(eh)) {
+		const struct click_event *event = cast_click_event(eh);
         return false;
 	}
     return false;
 }
 
 EVENT_LISTENER(MODULE, event_handler);
-EVENT_SUBSCRIBE(MODULE, button_event);
+EVENT_SUBSCRIBE(MODULE, click_event);
